d6527002d353a1932db4738f951783c83c09ca51
Vehan Hemsara <79366878+Hemsara@users.noreply.github.com>
2024-08-22 02:21:00 +0530

    Initial Commit

---
+++ brainy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  brainy
+//
+//  Created by Vehan Hemsara on 2024-08-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ brainy/brainyApp.swift
@@ -0,0 +1,17 @@
+//
+//  brainyApp.swift
+//  brainy
+//
+//  Created by Vehan Hemsara on 2024-08-22.
+//
+
+import SwiftUI
+
+@main
+struct brainyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
